54a963582a21c69ca13e059102598eb776a4eb24
Mark Anthony Vale <markavale1@gmail.com>
2021-01-15 02:16:44 +0800

    production

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask import Flask, request
 import telegram
 from telebot.ai import generate_smart_reply
 from telebot.credentials import bot_token, bot_user_name,URL
-
-
 global bot
 global TOKEN
 TOKEN = bot_token
@@ -29,13 +27,14 @@ def respond():
    bot.sendMessage(chat_id=chat_id, text=reply, reply_to_message_id=msg_id)
    # the first time you chat with the bot AKA the welcoming message
    if text == "/start":
+       
        # print the welcoming message
        bot_welcome = """
        Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
        """
        # send the welcoming message
        bot.sendChatAction(chat_id=chat_id, action="typing")
-    #    sleep(1.5)
+       sleep(1.5)
        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
 
 
@@ -43,12 +42,13 @@ def respond():
        try:
            # clear the message we got from any non alphabets
            text = re.sub(r"\W", "_", text)
+           print(text)
            # create the api link for the avatar based on http://avatars.adorable.io/
            url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
            # reply with a photo to the name the user sent,
            # note that you can send photos by url and telegram will fetch it for you
            bot.sendChatAction(chat_id=chat_id, action="upload_photo")
-        #    sleep(2)
+           sleep(2)
            bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
        except Exception:
            # if things went wrong
